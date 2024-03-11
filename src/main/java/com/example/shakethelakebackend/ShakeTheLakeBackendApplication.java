82735bbe9a84398d1c5ed823eb6be8bc00732e72
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-03-11 14:53:02 +0100

    initialized springboot backend

---
+++ src/main/java/com/example/shakethelakebackend/ShakeTheLakeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.shakethelakebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakeTheLakeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakeTheLakeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shakethelakebackend/ShakeTheLakeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shakethelakebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakeTheLakeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
