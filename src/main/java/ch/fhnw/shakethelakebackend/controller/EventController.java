132636eb6f9316fa578efa9682b5611ed5e7ad3d
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-04-17 18:46:04 +0200

    feat: added CorsConfig and delete endpoint for events

--- src/main/java/ch/fhnw/shakethelakebackend/controller/EventController.java
+++ src/main/java/ch/fhnw/shakethelakebackend/controller/EventController.java
@@ -9,6 +9,7 @@ import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,10 @@ public class EventController {
     @GetMapping()
     public List<Event> getAllEvents() {
         return eventService.getAllEvents();
+    }
+
+    @PutMapping("/{id}")
+    public void deleteEvent(@PathVariable Long id){
+        eventService.deleteEvent(id);
     }
 }
