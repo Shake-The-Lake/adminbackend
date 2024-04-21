a24a80f807711e1d72e40835f95827f19450a5ac
Burger Alain (IT-PTR-CEN2-YPT) <alain_nicolas.burger@sbb.ch>
2024-04-21 13:50:37 +0200

    fix: added deleteMapping instead of PutMapping in EventController

--- src/main/java/ch/fhnw/shakethelakebackend/controller/EventController.java
+++ src/main/java/ch/fhnw/shakethelakebackend/controller/EventController.java
@@ -6,10 +6,10 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,8 +36,8 @@ public class EventController {
         return eventService.getAllEvents();
     }
 
-    @PutMapping("/{id}")
-    public void deleteEvent(@PathVariable Long id){
+    @DeleteMapping("/{id}")
+    public void deleteEvent(@PathVariable Long id) {
         eventService.deleteEvent(id);
     }
 }
