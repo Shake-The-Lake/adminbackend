44fafd400b7017fc26fe35bbb0b90e6693a7ccd7
Noel Sollberger <sollberger.n@gmail.com>
2024-05-13 10:46:05 +0200

    feat: renamed security config and fixed style check violations

--- src/main/java/ch/fhnw/shakethelakebackend/config/SecurityConfig.java
+++ src/main/java/ch/fhnw/shakethelakebackend/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package ch.fhnw.shakethelakebackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,34 +17,45 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class Security {
+public class SecurityConfig {
 
     private static final String ADMIN = "ADMIN";
     private static final String USER = "USER";
+
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
 
         UserDetails user = User.withUsername("shakethelake-admin-user")
-                .password("$2a$10$AGklb1Ab/j.uxUk82HzMN.jnKwojzMEwDhbF3/YRvVogIKOkdHzNm")
-                .roles(ADMIN, USER)
-                .build();
+                .password("$2a$10$AGklb1Ab/j.uxUk82HzMN.jnKwojzMEwDhbF3/YRvVogIKOkdHzNm").roles(ADMIN, USER).build();
 
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        .requestMatchers("/timeslot/**", "/location/**", "/activitytype/**", "/event/**", "/booking/**", "/person/**", "/boat/**").hasRole(ADMIN)
-                        .requestMatchers("/public/**", "/auth/**").permitAll()
-                        .requestMatchers("/", "/error", "/csrf", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated())
+        return http.authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers("/timeslot/**",
+                                                 "/location/**",
+                                                 "/activitytype/**",
+                                                 "/event/**",
+                                                 "/booking/**",
+                                                 "/person/**",
+                                                 "/boat/**").hasRole(ADMIN)
+
+                                .requestMatchers("/public/**", "/auth/**").permitAll()
+
+                                .requestMatchers("/",
+                                                 "/error",
+                                                 "/csrf",
+                                                 "/swagger-ui.html",
+                                                 "/swagger-ui/**",
+                                                 "/v3/api-docs",
+                                                 "/v3/api-docs/**").permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable).build();
     }
 
     @Bean
