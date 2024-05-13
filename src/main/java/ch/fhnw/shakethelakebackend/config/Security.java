160f09af6c90e2f0fb6451fc1493fa071404df75
Noel Sollberger <sollberger.n@gmail.com>
2024-05-13 10:26:58 +0200

    feat: add basic auth config

---
+++ src/main/java/ch/fhnw/shakethelakebackend/config/Security.java
@@ -0,0 +1,55 @@
+package ch.fhnw.shakethelakebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+
+    private static final String ADMIN = "ADMIN";
+    private static final String USER = "USER";
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails user = User.withUsername("shakethelake-admin-user")
+                .password("$2a$10$AGklb1Ab/j.uxUk82HzMN.jnKwojzMEwDhbF3/YRvVogIKOkdHzNm")
+                .roles(ADMIN, USER)
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers("/timeslot/**", "/location/**", "/activitytype/**", "/event/**", "/booking/**", "/person/**", "/boat/**").hasRole(ADMIN)
+                        .requestMatchers("/public/**", "/auth/**").permitAll()
+                        .requestMatchers("/", "/error", "/csrf", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
