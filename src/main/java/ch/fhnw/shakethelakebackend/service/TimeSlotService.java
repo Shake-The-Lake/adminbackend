2ea4e583627168fc454b6cae8e9c2014bab23d39
Noel Sollberger <noel.sollberger@students.fhnw.ch>
2024-08-19 16:03:26 +0200

    feat: expander for timeslot
    story: https://gitlab.fhnw.ch/ip34-24bb/ip34-24bb_shakethelake/shakethelakedocs/-/issues/193
    
    code changes:
    * added expander for time slot

--- src/main/java/ch/fhnw/shakethelakebackend/service/TimeSlotService.java
+++ src/main/java/ch/fhnw/shakethelakebackend/service/TimeSlotService.java
@@ -1,11 +1,15 @@
 package ch.fhnw.shakethelakebackend.service;
 
+import ch.fhnw.shakethelakebackend.model.dto.ActivityTypeDto;
+import ch.fhnw.shakethelakebackend.model.dto.BoatDto;
 import ch.fhnw.shakethelakebackend.model.dto.CreateTimeSlotDto;
 import ch.fhnw.shakethelakebackend.model.dto.TimeSlotDto;
 import ch.fhnw.shakethelakebackend.model.entity.ActivityType;
 import ch.fhnw.shakethelakebackend.model.entity.Boat;
 import ch.fhnw.shakethelakebackend.model.entity.Booking;
 import ch.fhnw.shakethelakebackend.model.entity.TimeSlot;
+import ch.fhnw.shakethelakebackend.model.mapper.ActivityTypeMapper;
+import ch.fhnw.shakethelakebackend.model.mapper.BoatMapper;
 import ch.fhnw.shakethelakebackend.model.mapper.TimeSlotMapper;
 import ch.fhnw.shakethelakebackend.model.repository.TimeSlotRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -23,9 +28,12 @@ public class TimeSlotService {
 
     public static final String TIMESLOT_NOT_FOUND = "TimeSlot not found";
 
+    private final Expander expander;
     private final TimeSlotRepository timeSlotRepository;
     private final BoatService boatService;
     private final TimeSlotMapper timeSlotMapper;
+    private final BoatMapper boatMapper;
+    private final ActivityTypeMapper activityTypeMapper;
     private final ActivityTypeService activityTypeService;
 
     public TimeSlotDto createTimeSlot(CreateTimeSlotDto timeSlotDto) {
@@ -85,5 +93,30 @@ public class TimeSlotService {
 
     public List<TimeSlotDto> getAllTimeSlots() {
         return timeSlotRepository.findAll().stream().map(timeSlotMapper::toDto).collect(Collectors.toList());
+    }
+
+    public TimeSlotDto getTimeSlotDto(Long id, Optional<String> expand) {
+        TimeSlot timeSlot = getTimeSlot(id);
+        TimeSlotDto timeSlotDto = getTimeSlotDto(id);
+
+        expander.applyExpansion(expand, "activitytype", () -> {
+            ActivityTypeDto activityTypeDto = activityTypeMapper.toDto(timeSlot.getActivityType());
+            timeSlotDto.setActivityType(activityTypeDto);
+        });
+
+        expander.applyExpansion(expand, "boat", () -> {
+            BoatDto boatDto = boatMapper.toDto(timeSlot.getBoat());
+            timeSlotDto.setBoat(boatDto);
+        });
+
+        return timeSlotDto;
+    }
+
+    public List<TimeSlotDto> getAllTimeSlots(Optional<String> expand) {
+        List<TimeSlotDto> timeSlotDtos = getAllTimeSlots();
+
+        timeSlotDtos = timeSlotDtos.stream().map(timeSlot -> getTimeSlotDto(timeSlot.getId(), expand)).toList();
+
+        return timeSlotDtos;
     }
 }
