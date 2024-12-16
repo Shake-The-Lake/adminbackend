9bea40fcb72e8ee887d8a212965a891d3d23ce66
frithjof.hoppe <frithjof.hoppe@students.fhwn.ch>
2024-12-16 10:35:55 +0100

    fix: Correct checkstyle violations

--- src/main/java/ch/fhnw/shakethelakebackend/config/CorsConfig.java
+++ src/main/java/ch/fhnw/shakethelakebackend/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             // TODO Adjust for deployment
-            .allowedOrigins("http://localhost:3000", "https://shake-the-lake.ch", "http://localhost:8080")
+            .allowedOrigins("http://localhost:3000", "https://shake-the-lake.ch")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS").allowedHeaders("*")
             .allowCredentials(true);
     }
